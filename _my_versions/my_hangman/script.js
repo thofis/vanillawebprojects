cd74214ab6e122ce5df168f4616a8a4b4b6b4a11
Thomas Fischer <tf@media-nox.de>
2020-11-08 14:25:05 +0100

    working hangman version Squashed commit of the following:
    commit 26e08d4fc2d9e227257bfa67c9c95819e9ce15fc
    Author: Thomas Fischer <thomas.fischer@media-nox.de>
    Date:   Sat Oct 31 17:49:34 2020 +0100
    
        after hangman
    
    commit 7f936134b71cac853a28842add4c5fcbed880d25
    Merge: 338f07b 9a877f3
    Author: Thomas Fischer <thomas.fischer@media-nox.de>
    Date:   Fri Oct 30 15:10:33 2020 +0100
    
        Merge branch 'master' of github.com:thofis/vanillawebprojects
    
    commit 338f07bfa41174c10d487de5c3fd7f82492a96d7
    Author: Thomas Fischer <thomas.fischer@media-nox.de>
    Date:   Fri Oct 30 15:08:02 2020 +0100
    
        changes to form-validation

--- _my_versions/my_hangman/script.js
+++ _my_versions/my_hangman/script.js
@@ -1,33 +1,100 @@
+import terms from './terms.js'
+
 const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜß'
 
-const selectedLetters = []
 
 const keyContainer = document.getElementById('key-container')
 const figureParts = document.querySelectorAll('.figure-part')
+const categoryEl = document.getElementById('category')
+const termEl = document.getElementById('term')
 
-const wrongLetters = []
+const popup = document.getElementById('popup')
+const popupBtn = document.getElementById('btn-popup')
+const popupMessage = document.getElementById('msg-popup')
+
+let selectedLetters = []
+let wrongLetters = []
+let correctLetters = []
+
+let category = undefined;
+let term = undefined;
+let hiddenTerm = undefined
 
 console.log(figureParts)
 
 window.addEventListener('keydown', e => {
   const keyPressed = e.key !== 'ß' ? e.key.toUpperCase() : e.key
+  if (e.keyCode === 13 /* enter key */ && popup.style.display !== 'none') {
+    initNewGame()
+    return
+  }
   const correspondingButton = document.getElementById(`key-${keyPressed}`)
   if (!correspondingButton.disabled) {
     handleKeyPressed(correspondingButton)
   }
 })
 
+popupBtn.addEventListener('click', e => {
+  initNewGame()
+})
+
 function handleKeyPressed(element) {
   // console.log(element.innerText)
-  selectedLetters.push(element.innerText)
+
+  const selectedLetter = element.innerText
+
+  selectedLetters.push(selectedLetter)
   element.disabled = true
   element.classList.add('disabled')
   // console.log(selectedLetters)
-  wrongLetters.push(element.innerText)
+
+  checkGuess(selectedLetter)
+
+  updateHiddenterm()
+  updateFigure()
+
+  // won
+  if (hiddenTerm === term) {
+    console.log('Richtig geraten!')
+    // popup.className = ''
+    popup.classList.remove('lost')
+    popup.classList.add('won')
+    popup.style.display = 'flex';
+    popupMessage.innerText = "Richtig geraten!"
+  }
+  // lost
+  else if (figureParts.length == wrongLetters.length) {
+    console.log('Leider verloren.')
+    popup.classList.remove('won')
+    popup.classList.add('lost')
+    popup.style.display = 'flex';
+    correctLetters = letters;
+    popupMessage.innerText = "Leider verloren."
+    updateHiddenterm()
+  }
+
+
+}
+
+function updateFigure() {
   showFigureParts(wrongLetters.length)
 }
 
+function checkGuess(letter) {
+  if (term.indexOf(letter) > -1) {
+    correctLetters.push(letter)
+  } else {
+    wrongLetters.push(letter)
+  }
+}
+
+function updateHiddenterm() {
+  hiddenTerm = Array.from(term).map(ch => correctLetters.includes(ch) || !letters.includes(ch) ? ch : '_').join('')
+  termEl.innerText = hiddenTerm
+}
+
 function setupKeys() {
+  keyContainer.innerHTML = ''
   Array.from(letters).forEach(letter => {
     // console.log(`letter: ${letter}`)
     const buttonEl = document.createElement('button')
@@ -47,4 +114,54 @@ function showFigureParts(toIndex) {
   })
 }
 
-setupKeys()
+
+function getRandomCategory() {
+  // var item = items[Math.floor(Math.random() * items.length)];
+  return terms[Math.floor(Math.random() * terms.length)]
+}
+
+function getRandomTermOfCategory(category, termToRemove) {
+  const terms = category.terms
+  let randomTerm = null;
+  let undefinedCounter = 0;
+  while (!randomTerm && undefinedCounter < 10) {
+    randomTerm = terms[Math.floor(Math.random() * terms.length)]
+    undefinedCounter++;
+  }
+  return randomTerm
+}
+
+function removeTerm(category, termToRemove) {
+  category.terms = category.terms.filter(term => term !== termToRemove)
+
+}
+
+function setupTerm() {
+  category = getRandomCategory()
+  term = getRandomTermOfCategory(category)
+  // removeTerm(category, term)
+
+  hiddenTerm = Array.from(term).map(ch => letters.includes(ch)  ? '_' : ch).join('')
+
+  categoryEl.innerText = category.category
+  termEl.innerText = hiddenTerm
+
+
+  // console.log(`random category: ${category.category}`)
+  // console.log(`random term of this category: ${term}`)
+}
+
+
+function initNewGame() {
+  setupKeys()
+  setupTerm()
+  wrongLetters = []
+  selectedLetters = []
+  correctLetters = []
+  popup.style.display = 'none'
+  showFigureParts(0)
+}
+
+initNewGame()
+
+
---
+++ _my_versions/my_hangman/terms.js
@@ -0,0 +1,66 @@
+export default [
+  {
+    "category": "Gemeinde im Lkr. TIR",
+    "terms" : [
+      "WALDSASSEN",
+      "HUNDSBACH",
+      "BÄRNAU",
+      "WIESAU",
+      "TIRSCHENREUTH",
+      "KEMNATH",
+      "ERBENDORF",
+      "MITTERTEICH",
+      "HATZENREUTH",
+      "EGERTEICH",
+      "NEUSORG",
+      "WALDERSHOF",
+      "STEINMÜHLE",
+      "WONDREB",
+      "NEUALBENREUTH",
+      "GRIESBACH",
+    ]
+  },
+  {
+    "category": "TV-Sendungen",
+    "terms" : [
+      "DIE ROSENHEIM COPS",
+      "BARES FÜR RARES",
+      "SPORTSCHAU",
+      "DAS TRAUMSCHIFF",
+      "TAGESTHEMEN",
+      "DIE HEUTE-SHOW",
+      "PRESSECLUB",
+      "ANNE WILL",
+      "NEUES AUS DER ANSTALT",
+      "TATORT",
+      "BERLIN DIREKT",
+      "DIE KÜCHENSCHLACHT",
+      "DAS PERFEKTE DINNER",
+      "HART ABER FAIR",
+      "DAHOAM IS DAHOAM",
+      "BLICKPUNKT SPORT",
+      "MAISCHBERGER",
+      "THE VOICE OF GERMANY",
+      "GERMANY'S NEXT TOP MODEL"
+    ]
+  },
+  {
+    "category": "Speisepilze",
+    "terms" : [
+      "BIRKENPILZ",
+      "SPEISEMORCHEL",
+      "ROTKAPPE",
+      "GOLDRÖHRLING",
+      "BLAUPILZ",
+      "STEINPILZ",
+      "PFIFFERLING",
+      "PARASOL",
+      "STOCKSCHWÄMMCHEN",
+      "EGERLING",
+      "KRAUSE GLUCKE",
+      "PFIFFERLING",
+      "KUHMAUL",
+      "ROTFUßRÖHRLING"
+  ]
+  }
+];
